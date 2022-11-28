4d4ec779b89ea1fcd099b34aed93c799855093da
MicSG <michelsgalvao@gmail.com>
2022-11-28 01:25:57 -0300

    Create credenciais.py

---
+++ credenciais.py
@@ -0,0 +1,4 @@
+HOST = "*******" # Nome do servidor do seu plano de hospedagem
+PORT = 587 # A porta SSL ou TTL
+MY_ADDRESS = "**********" # o e-mail remetente para enviar mensagens
+PASSWORD = "*************" # a senha do e-mail remetente (MY_ADDRESS) 
